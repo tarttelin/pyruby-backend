910819aa123622abbe633a56381b4befa57ee0da
Chris Tarttelin <ctarttelin@equalexperts.com>
2022-03-01 14:16:07 +0000

    Make the tests run in cloudbuild.

--- tests/test_user_repo.py
+++ tests/test_user_repo.py
@@ -2,7 +2,7 @@ from pyruby_backend.repositories import UserRepo
 from pyruby_backend.models import User
 import requests
 from requests.exceptions import HTTPError
-import json
+import os
 
 def test_store_new_user():
     repo = UserRepo()
@@ -13,14 +13,21 @@ def test_store_new_user():
 
 
 def test_login():
-    request_url = "http://localhost:9099/identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=dummy_key"
     data = {"email": "chris@tarttelin.co.uk", "password": "password", "returnSecureToken": True}
+    _auth_command("signUp", data)
+    login = _auth_command("signInWithPassword", data)
 
-    resp = requests.post(request_url, json=data)
+    print(login)
+
+
+def _auth_command(command, payload):
+    request_url = f"http://{os.getenv('FIREBASE_AUTH_EMULATOR_HOST', 'firebase:9099')}/identitytoolkit.googleapis.com/v1/accounts:{command}?key=dummy_key"
+
+    resp = requests.post(request_url, json=payload)
 
     try:
         resp.raise_for_status()
     except HTTPError as e:
         raise HTTPError(e, resp.text)
 
-    print(resp.json())
+    return resp.json()
